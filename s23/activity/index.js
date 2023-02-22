78c89f7990d35bcb437a50d446b25508ae88593f
@obligado86 <obligado86@yahoo.com>
2023-02-22 16:50:03 +0800

    add activity code s23

---
+++ s23/activity/index.js
@@ -0,0 +1,62 @@
+// console.log("Hello World");
+
+//Follow the property names and spelling given in the google slide instructions.
+let trainer = {
+	name: "Ash Ketchum",
+	age: 10,
+	pokemon: ["Pikachu", "Charizard", "Squirtle", "Bulbasaur"],
+	talk
+}
+
+// Create an object called trainer using object literals
+
+// Initialize/add the given object properties and methods
+
+// Properties
+
+// Methods
+
+// Check if all properties and methods were properly added
+
+
+// Access object properties using dot notation
+
+// Access object properties using square bracket notation
+
+// Access the trainer "talk" method
+
+
+// Create a constructor function called Pokemon for creating a pokemon
+
+
+// Create/instantiate a new pokemon
+
+
+// Create/instantiate a new pokemon
+
+
+// Create/instantiate a new pokemon
+
+
+// Invoke the tackle method and target a different object
+
+
+// Invoke the tackle method and target a different object
+
+
+
+
+
+
+
+
+//Do not modify
+//For exporting to test.js
+try{
+	module.exports = {
+		trainer,
+		Pokemon 
+	}
+} catch(err) {
+
+}
--- s23/discussion/index.js
+++ s23/discussion/index.js
@@ -91,3 +91,149 @@ let nikeShoes = new shoes("Nike", 2023, "highCut");
 console.log(nikeShoes);
 let adidasShoes = new shoes("addidas", 2022, "lowcut");
 console.log(adidasShoes);
+
+
+console.log("=======================================");
+// accessing object property
+
+console.log("result from dot notation " + laptopJohn.brand);
+console.log("result from square bracket notation " + laptopJohn["brand"]);
+
+
+// accessing array objects
+let deviceArr = [laptopJohn, laptopOwen];
+console.log(deviceArr);
+
+//dot notation
+console.log(deviceArr[0].brand);
+// square bracket notation
+console.log(deviceArr[0]["manifactureDate"]);
+
+// initializing/ adding/deleting/reassigning object properties
+
+// crud operations
+
+//initializing object
+
+let car = {};
+console.log(car);
+
+// adding object
+
+// using dot notation
+car.name = "Honda civic";
+console.log("result in adding properties using dot notation:")
+console.log(car);
+
+//using square bracket notation for adding object properties
+/*
+		    - While using the square bracket will allow access to spaces when assigning property names to make it easier to read, this also makes it so that object properties can only be accesssed using the square bracket notation
+		    - This also makes names of object properties to not follow commonly used naming conventions for them
+		*/
+
+car["manufacture date"] = 2019;
+console.log(car["manufacture date"]);
+console.log(car["Manufacture date"]); // undefined must be same with the declaired name
+console.log(car.manifactureDate);
+
+console.log("result from adding properties using squre braket notation:");
+
+console.log(car);
+console.log("================deleting==============");
+// deleting object properties
+
+delete car["manufacture date"];
+console.log("result from deleting properties");
+console.log(car);
+
+// reassinging object properties (updating)
+
+console.log("================reassinging==============");
+
+car.name = "Tesla";
+console.log("result form reassigning properties:");
+console.log(car);
+
+// object methods
+
+/*
+
+	This method is a function which is stored in an object property. Ther are also functions and one of the key difference that they have is that methods are functions related to a specific object.
+
+*/
+
+let person = {
+	name: "Pedro",
+	age: "25",
+	talk: function(){
+		console.log("hello! ako si " + this.name)
+	}
+};
+
+console.log(person);
+console.log("result for object method");
+person.talk();
+
+person.walk = function(){
+	console.log(this.name + " have walked 25 steps forward");
+};
+
+person.walk();
+
+let friend = {
+	firstname: "maria",
+	lastName: "dela cruz",
+	address: {
+		city: "austin, texas",
+		country: "US"
+	},
+	emails: ["maria143@gmail.com", "maria_bossxz@mail.com"],
+	introduce: function(){
+		console.log("hello! My name is " + this.firstname + " " + this.lastName + "." + " I live in " + this.address.city + ", " + this.address.country + ".");
+	}
+};
+
+friend.introduce();
+
+let myPokemon = {
+	name: "Charizard",
+	level: 3,
+	health: 100,
+	attack: 50,
+	tackle: function(){
+		console.log("this pokemon tackled targeted pokemon");
+		console.log("targetPokemon's health is now reduced to _targetPokemonhealth_");
+	},
+	faint: function(){
+		console.log("pokemon fainted.");
+	}
+}
+
+console.log(myPokemon);
+
+// creating an object constructor instead wil healp with this process
+function Pokemon(name, level){
+	this.name = name;
+	this.level = level;
+	this.health = 2 * level;
+	this.attack = level;
+
+	//methods
+
+	this.tackle = function(target){
+		console.log(this.name + " tackled " + target.name);
+		console.log(target.name + "'s health is now reduced to " + target.health);
+	};
+	this.faint = function(){
+		console.log(this.name + " fainted.");
+	}
+}
+
+//creates new instances of the "pokemon" object each with thie unique properties
+
+let pikachu = new Pokemon("pikachu", 16);
+let charmander = new Pokemon("Charmander", 8);
+
+pikachu.tackle(charmander);
+ 
+
