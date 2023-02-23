632280fb1550d7b69f88563713d325761142e19a
@obligado86 <obligado86@yahoo.com>
2023-02-23 16:49:39 +0800

    add activity code s25

--- s25/activity/index.js
+++ s25/activity/index.js
@@ -1,6 +1,6 @@
 let users = `[
     {
-      "id": {
+      "id": 1,
       "name": "Leanne Graham",
       "username": "Bret",
       "email": "Sincere@april.biz",
@@ -8,8 +8,7 @@ let users = `[
         "street": "Kulas Light",
         "suite": "Apt. 556",
         "city": "Gwenborough",
-        "zipcode": "92998-3874"
-        },
+        "zipcode": "92998-3874",
         "geo": {
           "lat": "-37.3159",
           "lng": "81.1496"
