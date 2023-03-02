2dae55aac5c7e107638417f9066bd337fe3b0fea
@obligado86 <obligado86@yahoo.com>
2023-03-02 17:36:15 +0800

    add activity code s33

--- s33/activity/script.js
+++ s33/activity/script.js
@@ -18,7 +18,15 @@ fetch("https://jsonplaceholder.typicode.com/todos")
 
 //5. Create a fetch request using the GET method that will retrieve a single to do list item from JSON Placeholder API.
 
+fetch("https://jsonplaceholder.typicode.com/todos/1")
+	.then((response) => response.json())
+	.then((json) => console.log(json));
+
 //6. Using the data retrieved, print a message in the console that will provide the title and status of the to do list item.
+
+fetch("https://jsonplaceholder.typicode.com/todos/1")
+	.then((response) => response.json())
+	.then((json) => console.log(`The Item "${json.title}" on the list has a status of ${json.completed}`));
 
 //7. Create a fetch request using the POST method that will create a to do list item using the JSON Placeholder API.
 
