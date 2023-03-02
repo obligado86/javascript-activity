82112b8bf2e4a0626a1e3aad57e366032228a53e
@obligado86 <obligado86@yahoo.com>
2023-03-02 18:15:07 +0800

    add activity code s33

--- s33/activity/script.js
+++ s33/activity/script.js
@@ -30,7 +30,36 @@ fetch("https://jsonplaceholder.typicode.com/todos/1")
 
 //7. Create a fetch request using the POST method that will create a to do list item using the JSON Placeholder API.
 
+fetch("https://jsonplaceholder.typicode.com/todos", {
+	method: "POST",
+	headers: {
+		"Content-type": "application/json"
+	},
+	body: JSON.stringify({
+		completed: false,
+		title: "Created To Do List Item",
+		userId: 1
+	})
+}).then(response => response.json())
+	.then(json => console.log(json));
+
+
 //8. Create a fetch request using the PUT method that will update a to do list item using the JSON Placeholder API.
+
+fetch("https://jsonplaceholder.typicode.com/todos/1", {
+	method: "PUT",
+	headers: {
+		"Content-type": "application/json"
+	},
+	body: JSON.stringify({
+		dateCompleted: "Pending",
+		description: "To update the my to do list with a different data structure",
+		status: "Pending",
+		title: "Updated To Do List Item",
+		userId: 1
+	})
+}).then(response => response.json())
+	.then(json => console.log(json));
 
 /*9. Update a to do list item by changing the data structure to contain the following properties:
 - Title
@@ -39,8 +68,55 @@ fetch("https://jsonplaceholder.typicode.com/todos/1")
 - Date Completed
 - User ID*/
 
+fetch("https://jsonplaceholder.typicode.com/todos/1", {
+	method: "PATCH",
+	headers: {
+		"Content-type": "application/json"
+	},
+	body: JSON.stringify({
+		completed: false,
+		dateCompleted: "07/09/21",
+		status: "Complete",
+		title: "delectus aut autem",
+		userId: 1
+	})
+}).then(response => response.json())
+	.then(json => console.log(json));
+
+
 //10. Create a fetch request using the PATCH method that will update a to do list item using the JSON Placeholder API.
+
+fetch("https://jsonplaceholder.typicode.com/todos/1", {
+	method: "PATCH",
+	headers: {
+		"Content-type": "application/json"
+	},
+	body: JSON.stringify({
+		completed: false,
+		dateCompleted: "08/09/21",
+		status: "Pending",
+		title: "delectus aut autem",
+		userId: 1
+	})
+}).then(response => response.json())
+	.then(json => console.log(json));
 
 //11. Update a to do list item by changing the status to complete and add a date when the status was changed.
 
+fetch("https://jsonplaceholder.typicode.com/todos/1", {
+	method: "PUT",
+	headers: {
+		"Content-type": "application/json"
+	},
+	body: JSON.stringify({
+		dateCompleted: "08/15/21",
+		status: "Complete",
+	})
+}).then(response => response.json())
+	.then(json => console.log(json));
+
 //12. Create a fetch request using the DELETE method that will delete an item using the JSON Placeholder API.
+
+fetch("https://jsonplaceholder.typicode.com/todos/1", {
+	method: "DELETE",
+});
