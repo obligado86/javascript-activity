a1e803706658c24a96d6321e22a917f4072207b5
@obligado86 <obligado86@yahoo.com>
2023-03-07 08:00:09 +0800

    add activity code s37

--- s37-41/models/Course.js
+++ s37-41/models/Course.js
@@ -35,46 +35,6 @@ const courseSchema = new mongoose.Schema({
 	]
 });
 
-const userSchema = new mongoose.Schema({
-	firstName: {
-		type: String,
-		required: [true, "firstName is required"]
-	},
-	lastName: {
-		type: String,
-		required: [true, "lastName is required"]
-	},
-	email: {
-		type: String,
-		required: [true, "email is required"]
-	},
-	password: {
-		type: String,
-		required: [true, "password is required"]
-	},
-	isAdmin: {
-		type: Boolean,
-		default: false
-	},
-	mobileNo: {
-		type: String,
-		required: [true, "mobileNo is required"]
-	},
-	endrollments: [{
-		courseId: {
-			type: String,
-			required: [true, "courseId is required"]
-		},
-		endrolledOn: {
-			type: Date,
-			default: new Date()
-		},
-		status: {
-			type: String,
-			default: "Endrolled"
-		}
-	}]
-})
 
 module.exports = mongoose.model("Course", courseSchema);
 
